ef082e9b9e969727beedcda1c60da0815326ba4f
Martijn Braam <martijn@brixit.nl>
2014-04-04 12:57:44 +0200

    Initial commit

---
+++ Asana.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martijn
+ * Date: 4/4/14
+ * Time: 12:57 PM
+ */
+
+namespace PhpAsana;
+
+
+class Asana {
+
+} 
---
+++ Project.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martijn
+ * Date: 4/4/14
+ * Time: 12:53 PM
+ */
+
+namespace PhpAsana;
+
+
+class Project {
+
+} 
