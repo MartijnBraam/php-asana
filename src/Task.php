e2e370f55f5d0334e0344ab1bf4e0b40b84bf921
Martijn Braam <martijn@brixit.nl>
2014-04-06 16:13:39 +0200

    Added all properties to Task object

--- src/Task.php
+++ src/Task.php
@@ -10,6 +10,68 @@ namespace PhpAsana;
 
 
 class Task {
+  public $id;
+  public $name;
+
+  private $loaded = false;
+  private $assignee;
+  private $createdAt;
+  private $isCompleted;
+  private $completedAt;
+  private $dueOn;
+  private $followers;
+  private $modifiedAt;
+  private $notes;
+  private $parentTask;
+
+  private $asanaconnection;
+
+  private $lazyloaded = array('assignee', 'createdAt', 'isCompleted', 'completedAt', 'dueOn', 'followers', 'modifiedAt',
+    'notes', 'parentTask');
+
+  public function __construct(Array $meta, Asana $asanaconnection){
+    $this->id = $meta['id'];
+    $this->name = $meta['name'];
+    $this->asanaconnection = $asanaconnection;
+  }
+
+  public function __get($name){
+    if(in_array($name, $this->lazyloaded)){
+      if($this->loaded){
+        return $this->$name;
+      }else{
+        $this->load();
+        return $this->$name;
+      }
+    }
+    return null;
+  }
+
+  public function __set($name, $value){
+    if(in_array($name, $this->lazyloaded)){
+      $this->$name = $value;
+    }
+  }
+
+  private function load(){
+    $alldata = $this->asanaconnection->asanaRequest('GET', 'tasks/' . $this->id);
+    $this->createdAt = new \DateTime($alldata['data']['created_at']);
+    $this->modifiedAt = new \DateTime($alldata['data']['modified_at']);
+    $this->completedAt = new \DateTime($alldata['data']['completed_at']);
+    $this->dueOn = new \DateTime($alldata['data']['due_on']);
+    $this->assignee = $alldata['data']['assignee'];
+    $this->isCompleted = $alldata['data']['completed'];
+    $this->followers = $alldata['data']['followers'];
+    $this->notes = $alldata['data']['notes'];
+    if($alldata['data']['parent']===null){
+      $this->parentTask = null;
+    }else{
+      $this->parentTask = new Task($alldata['data']['parent'], $this->asanaconnection);
+    }
+    $this->loaded = true;
+  }
+
+
   public function save(){
     //TODO: implement save()
   }
